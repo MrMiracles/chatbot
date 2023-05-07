19a40eb1ee5bb9451eee68375146a3baaff6936d
Mr. Miracles <website@mrmiracles.nl>
2023-05-07 19:55:19 +0200

    Added stats table

--- database.sql
+++ database.sql
@@ -35,6 +35,20 @@ CREATE TABLE `responses` (
   `response` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
+-- Table structure for table `stats`
+--
+
+CREATE TABLE `stats` (
+  `id` int UNSIGNED NOT NULL,
+  `session_id` int UNSIGNED NOT NULL,
+  `group_id` smallint UNSIGNED NOT NULL,
+  `type` enum('keyword','response') DEFAULT NULL,
+  `type_id` int UNSIGNED DEFAULT NULL,
+  `score` tinyint DEFAULT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
 
 
 --
@@ -56,6 +70,12 @@ ALTER TABLE `keyword_x_responses`
 ALTER TABLE `responses`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `stats`
+--
+ALTER TABLE `stats`
+  ADD PRIMARY KEY (`id`);
+
 
 
 --
@@ -68,4 +88,10 @@ ALTER TABLE `keywords`
 -- AUTO_INCREMENT for table `responses`
 --
 ALTER TABLE `responses`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `stats`
+--
+ALTER TABLE `stats`
   MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
